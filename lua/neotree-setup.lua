27f7a6529d6615c1918c440148cc0df49fe4cb09
Felipe <felipemouradev@gmail.com>
2023-06-07 13:37:51 -0500

    backthe file for nvimtree

--- lua/neotree-setup.lua
+++ lua/neotree-setup.lua
@@ -1,4 +1,10 @@
-require("neo-tree").setup({
+local present, neotree = pcall(require, "neo-tree")
+
+if not present then
+  return
+end
+
+neotree.setup({
   source_selector = {
     winbar = true,
     content_layout = "center",
