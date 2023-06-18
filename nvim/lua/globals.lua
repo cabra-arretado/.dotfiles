ea6b4a692128f3908115e61f062b2a48e73de708
Felipe <felipemouradev@gmail.com>
2023-06-17 22:37:47 -0500

    with dotenv

--- nvim/lua/globals.lua
+++ nvim/lua/globals.lua
@@ -3,3 +3,21 @@ P = function(value)
   print(vim.inspect(value))
   return value
 end
+
+function Set_env()
+  -- Function to set environment variables from .env file
+    local env = vim.env
+    local env_file = os.getenv("HOME") .. '/dotfiles/nvim/.env'
+    local file = io.open(env_file, "r")
+    if file == nil then
+        vim.notify("Environment file not found: " .. env_file)
+        return
+    end
+    for line in file:lines() do
+        local key, value = line:match("^([^=]+)=(.*)$")
+        if key ~= nil then
+            env[key] = value
+        end
+    end
+    file:close()
+end
