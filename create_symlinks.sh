6318950e1e4b114949efcfe00150ab65833f08ba
Felipe <felipemouradev@gmail.com>
2023-06-17 21:47:50 -0500

    create new folder structure

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -6,37 +6,19 @@ read answer
 
 if [ $answer == "yes" ]; then
 
+    DOTFILES_REPO="~/dotfiles"
     PATH_VIMRC="$HOME/.vimrc"
-    PATH_NVIM="$HOME/.config/nvim"
-    PATH_NVIMCONFIG="$HOME/.config/nvim/init.lua"
     PATH_TMUX="$HOME/.tmux.conf"
-
-    PATH_OLD="$HOME/.original_dotfiles"
-    DOTFILES_REPO="$HOME/dotfiles"
-
-    # Check if the PATH_OLD exists, if not, create it
-    if [ ! -d "$PATH_OLD" ]; then
-	mkdir $PATH_OLD
-    fi 
-
-    # check if PATH_NVIM exists, if not, create it
-    if [ ! -d "$PATH_NVIM" ]; then
-	mkdir $PATH_NVIM
-    fi
-
-    # try to move the old files to the PATH_OLD
-    mv $PATH_VIMRC $PATH_OLD
-    mv $PATH_NVIMCONFIG $PATH_OLD
-    mv $PATH_TMUX $PATH_OLD
+    DOTCONFIG="$HOME/.config/"
 
     # create the symlinks
     ln -sf $DOTFILES_REPO/vimrc $PATH_VIMRC
     ln -sf $DOTFILES_REPO/tmux.conf $PATH_TMUX
 
     # NeoVim
-    ln -sf $DOTFILES_REPO/init.lua $PATH_NVIMCONFIG
-    sudo ln -sf $DOTFILES_REPO/lua $PATH_NVIM/lua
+    # Create symlink for NeoVim
+    ln -sf ~/dotfiles/nvim ~/.config/
 
-    echo "Symlinks created successfully. Old files moved to $PATH_OLD. Enjoy my NeoVim config!"
+    echo "Symlinks created successfully. Enjoy my NeoVim config!"
 fi
 
