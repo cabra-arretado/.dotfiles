b5a831dc8fe13b08255878f8b0d3d766b75fecd2
Felipe <felipemouradev@gmail.com>
2023-07-14 12:18:47 -0500

    fix the ; problem

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -109,17 +109,17 @@ return require('lazy').setup({
     opts = {
       modes = {
         char = {
-          keys = { [";"] = nil , [","] = nil }
-          -- keys = { "f", "F", "t", "T" },
+          -- keys = { [";"] = nil , [","] = nil }
+          keys = { "f", "F", "t", "T", "," },
         }
       }
     },
     keys = {
-      -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      -- { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
   {
