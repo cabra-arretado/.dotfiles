cb57fb0751edd7d81795c55ab950c9b8774b63bd
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-11-04 23:35:55 -0500

    comments

--- nvim/.config/nvim/after/plugin/luasnip.lua
+++ nvim/.config/nvim/after/plugin/luasnip.lua
@@ -1,4 +1,6 @@
 -- Video: https://www.youtube.com/watch?v=Dn800rlPIho
+-- Example: https://github.com/tjdevries/config_manager/blob/master/xdg_config/nvim/after/plugin/luasnip.lua
+--
 -- local ok, luasnip = pcall(require, "luasnip")
 
 -- if not ok then
