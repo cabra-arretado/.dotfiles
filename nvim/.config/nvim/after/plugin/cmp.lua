01530e84d1519cf5013ceeae4a72591f7ebb6daf
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-11-05 20:46:31 -0600

    cmp working

--- nvim/.config/nvim/after/plugin/cmp.lua
+++ nvim/.config/nvim/after/plugin/cmp.lua
@@ -8,12 +8,7 @@ if not l_present then
   return
 end
 
-
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 -- Global setup.
 cmp.setup {
   snippet = {
@@ -22,6 +17,7 @@ cmp.setup {
     end,
   },
   experimental = {
+    ghost_text = true,
     native_menu = false
   },
   window = {
@@ -29,30 +25,10 @@ cmp.setup {
     -- documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_prev_item()
-    --   elseif luasnip.jumpable(-1) then
-    --     luasnip.jump(-1)
-    --   else
-    --     fallback()
-    --   end
-    -- end, { "i", "s" }),
+    ['<M-k>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<M-j>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<M-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<M-Space>"] = cmp.mapping.complete(),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -60,25 +60,32 @@ return require('lazy').setup({
   { 'akinsho/bufferline.nvim' },
   { 'nvim-lualine/lualine.nvim' },
   { 'nvim-tree/nvim-web-devicons' },
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-  -- TODO: Create a config file if necessary
-  -- {
-  --   "lukas-reineke/indent-blankline.nvim",
-  --   event = { "BufReadPost", "BufNewFile" },
-  --   opts = {
-  --     char = "│",
-  --     filetype_exclude = {
-  --       "help",
-  --       "neo-tree",
-  --       "lazy",
-  --       "mason",
-  --       "notify",
-  --       "toggleterm",
-  --     },
-  --     show_trailing_blankline_indent = false,
-  --     show_current_context = false,
-  --   },
-  -- },
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    dependencies = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {}
+  },
   {
     'hrsh7th/nvim-cmp',
     lazy = true,
