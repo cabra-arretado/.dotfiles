5fbfee494cac6efd71d4c7cfb726444707618351
Felipe M <80273544+cabra-arretado@users.noreply.github.com>
2024-04-16 15:31:34 -0500

    remove filetype from nvim

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -10,6 +10,7 @@ lualine.setup {
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
     disabled_filetypes = {
+      'NvimTree',
       statusline = {},
       winbar = {},
     },
