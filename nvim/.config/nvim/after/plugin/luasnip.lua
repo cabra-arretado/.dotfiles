e04955017c7b2ab067a51218fa2fcf6d5ec73054
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2024-07-05 18:14:39 -0500

    adds print for snipets for lua and ts

--- nvim/.config/nvim/after/plugin/luasnip.lua
+++ nvim/.config/nvim/after/plugin/luasnip.lua
@@ -36,57 +36,50 @@ vim.keymap.set('n', '<leader><leader>s', '<cmd>source ~/.config/nvim/after/plugi
 -- End TJ ----
 ---------------------------------------
 ---------------------------------------
-
--- local s = ls.snippet
--- local sn = ls.snippet_node
--- local isn = ls.indent_snippet_node
--- local t = ls.text_node
--- local i = ls.insert_node
--- local f = ls.function_node
--- local c = ls.choice_node
--- local d = ls.dynamic_node
--- local r = ls.restore_node
--- local events = require("luasnip.util.events")
--- local ai = require("luasnip.nodes.absolute_indexer")
--- local extras = require("luasnip.extras")
--- local l = extras.lambda
--- local rep = extras.rep
--- local p = extras.partial
--- local m = extras.match
--- local n = extras.nonempty
--- local dl = extras.dynamic_lambda
--- local fmt = require("luasnip.extras.fmt").fmt
--- local fmta = require("luasnip.extras.fmt").fmta
--- local conds = require("luasnip.extras.expand_conditions")
--- local postfix = require("luasnip.extras.postfix").postfix
--- local parse = require("luasnip.util.parser").parse_snippet
--- local ms = ls.multi_snippet
--- local k = require("luasnip.nodes.key_indexer").new_key
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
+local extras = require("luasnip.extras")
+local l = extras.lambda
+local rep = extras.rep
+local p = extras.partial
+local m = extras.match
+local n = extras.nonempty
+local dl = extras.dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local conds = require("luasnip.extras.expand_conditions")
+local postfix = require("luasnip.extras.postfix").postfix
+local types = require("luasnip.util.types")
+local parse = require("luasnip.util.parser").parse_snippet
+local ms = ls.multi_snippet
+local k = require("luasnip.nodes.key_indexer").new_key
 local types = require("luasnip.util.types")
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
--- ls.snippets = {
---   all = {
---     ls.parser.parse_snippet({
---       "pl",
---       "-- helllo"
---     })
---   }
--- }
---   all = {
---     ls.parser.parse_snippet({
---       "pl",
---       "-- helllo"
---     })
---   }
---   -- lua = {
---   --   ls.parser.parse_snippet({
---   --     "pp",
---   --     "print(${1:})"
---   --   })
---   -- }
--- }
+ls.add_snippets("lua", {
+    s("pp", {
+      t("print("),
+      i(1),
+      t(")"),
+    })
+})
+ls.add_snippets("typescript", {
+    s("pp", {
+      t("console.log("),
+      i(1),
+      t(")"),
+    })
+})
 
 ls.config.set_config {
   history = false,
