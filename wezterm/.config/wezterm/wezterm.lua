b7676f8efd4082ae6ccf23ce80f0d15728d6569a
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2024-07-06 00:04:44 -0500

    just adding upping the font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,9 +2,8 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-
 config.color_scheme = 'Batman'
-config.font_size = 15
+config.font_size = 17
 config.window_decorations = "RESIZE"
 
 return config
