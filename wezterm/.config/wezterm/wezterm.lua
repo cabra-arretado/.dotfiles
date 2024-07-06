5cebf4cbc9f444bdc1b554b3207cb2f64a174363
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2024-07-06 00:35:05 -0500

    first draft

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,4 +7,25 @@ config.font_size = 17
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
+config.leader = { key = ",", mods = "CTRL", timeout_milliseconds = 1000 }
+
+-- Define key bindings with the leader key
+config.keys = {
+  -- Pane splitting
+  { key = "|", mods = "LEADER", action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+  { key = "-", mods = "LEADER", action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
+
+  -- Pane navigation
+  { key = "h", mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Left" } },
+  { key = "l", mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Right" } },
+  { key = "k", mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Up" } },
+  { key = "j", mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Down" } },
+
+  -- Tab management
+  { key = "t", mods = "LEADER", action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
+  { key = "w", mods = "LEADER", action = wezterm.action { CloseCurrentTab = { confirm = true } } },
+  -- { key = "n", mods = "LEADER", action = wezterm.action { ActivateTabRelative = 1 } },
+  -- { key = "p", mods = "LEADER", action = wezterm.action { ActivateTabRelative = -1 } },
+}
+
 return config
